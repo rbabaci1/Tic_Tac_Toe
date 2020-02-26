651b179468333710e53e9b52ea7a7ad1ef86c0ad
rabah babaci <4rabah@gmail.com>
2020-02-26 13:40:02 -0800

    add a click handler to the game

--- app/src/components/Game/Game.js
+++ app/src/components/Game/Game.js
@@ -5,25 +5,34 @@ import Square from '../Square/Square';
 
 export default function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [nextPlayer, setNextPlayer] = useState(true);
+
+  const clickHandler = value => {
+    const updatedSquares = [...squares];
+
+    updatedSquares[value] = nextPlayer ? 'X' : 'O';
+    setSquares(updatedSquares);
+    setNextPlayer(!nextPlayer);
+  };
 
   return (
     <div className='game-board'>
       <div className='game-row'>
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        <Square value={0} clickHandler={clickHandler} />
+        <Square value={1} clickHandler={clickHandler} />
+        <Square value={2} clickHandler={clickHandler} />
       </div>
 
       <div className='game-row'>
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        <Square value={3} clickHandler={clickHandler} />
+        <Square value={4} clickHandler={clickHandler} />
+        <Square value={5} clickHandler={clickHandler} />
       </div>
 
       <div className='game-row'>
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={'X'} />
+        <Square value={6} clickHandler={clickHandler} />
+        <Square value={7} clickHandler={clickHandler} />
+        <Square value={8} clickHandler={clickHandler} />
       </div>
     </div>
   );
