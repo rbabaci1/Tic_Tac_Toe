e2efa5215368c411ba31b1746f493274836eea2f
rabah babaci <4rabah@gmail.com>
2020-02-26 13:09:39 -0800

    render all the squares in Game

--- app/src/components/Game/Game.js
+++ app/src/components/Game/Game.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Square from '../Square/Square';
 
 export default function Game() {
-  return <div>Game</div>;
+  return (
+    <div className='game-board'>
+      <Square />
+
+      <Square />
+
+      <Square />
+    </div>
+  );
 }
--- app/src/components/Square/Square.js
+++ app/src/components/Square/Square.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Square() {
-  return <div></div>;
+  return <div className='game-row'></div>;
 }
