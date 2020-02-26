318ea2e74809386f3635363e29b3184f1cb96fe6
rabah babaci <4rabah@gmail.com>
2020-02-26 13:04:05 -0800

    render Game in App

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Game from './components/Game/Game';
 
 import './App.css';
 
 function App() {
-  return <div className='App'>Test</div>;
+  return (
+    <div className='App'>
+      <Game />
+    </div>
+  );
 }
 
 export default App;
