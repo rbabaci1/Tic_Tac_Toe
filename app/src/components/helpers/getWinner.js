70ceb64ac146d90ec88750b5a6ac7e72d283b0df
rabah babaci <4rabah@gmail.com>
2020-02-26 16:55:20 -0800

    add the getWinner function

--- app/src/components/helpers/getWinner.js
+++ app/src/components/helpers/getWinner.js
@@ -1,5 +1,5 @@
 const getWinner = squares => {
-  const winnigLines = [
+  const winingLines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -9,4 +9,14 @@ const getWinner = squares => {
     [0, 4, 8],
     [2, 4, 6]
   ];
+
+  for (let i = 0; i < winingLines.length; i++) {
+    const [a, b, c] = winingLines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 };
